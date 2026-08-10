1e03bc0bedb85bc45ee4db5763c9894c08d31411
Renujan <roshanrenujan20020426@gmail.com>
2026-08-10 14:43:02 +0530

    Configure custom user authentication model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ['username', 'email', 'currency', 'is_staff', 'is_active']
+    fieldsets = UserAdmin.fieldsets + (
+        ('Finance Info', {'fields': ('currency',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Finance Info', {'fields': ('currency',)}),
+    )
