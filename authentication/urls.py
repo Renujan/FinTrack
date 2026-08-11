5b00d8446896efc4842c13155f9c6cb14d736385
Renujan <roshanrenujan20020426@gmail.com>
2026-08-11 09:55:07 +0530

    05 Implement user registration endpoint

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from authentication.views import RegisterView
 
 app_name = 'authentication'
 
-urlpatterns = []
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+]
--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,25 @@
-from rest_framework import status
+from rest_framework import status, generics, permissions
 from rest_framework.response import Response
+from authentication.serializers import RegisterSerializer
 
-# Authentication views will be defined here
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response(
+            {
+                "message": "User registered successfully",
+                "user": {
+                    "id": user.id,
+                    "username": user.username,
+                    "email": user.email,
+                    "currency": user.currency
+                }
+            },
+            status=status.HTTP_201_CREATED
+        )
