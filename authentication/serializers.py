84a31501fe970908a36e9daecf6cd68cfa17d143
Renujan <roshanrenujan20020426@gmail.com>
2026-08-11 09:54:57 +0530

    04 Create user registration serializer

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,3 +1,45 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 
-# Authentication serializers will be defined here
+User = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password]
+    )
+    password_confirm = serializers.CharField(
+        write_only=True,
+        required=True
+    )
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password', 'password_confirm', 'currency')
+        extra_kwargs = {
+            'email': {'required': True},
+            'currency': {'required': False, 'default': 'LKR'},
+        }
+
+    def validate_email(self, value):
+        if User.objects.filter(email__iexact=value).exists():
+            raise serializers.ValidationError("A user with this email already exists.")
+        return value
+
+    def validate(self, attrs):
+        if attrs.get('password') != attrs.get('password_confirm'):
+            raise serializers.ValidationError({"password_confirm": "Passwords do not match."})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            currency=validated_data.get('currency', 'LKR')
+        )
+        return user
