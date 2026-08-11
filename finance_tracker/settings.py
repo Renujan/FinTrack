92401c140fad86759bf0ef5e25afecbde2a8071b
Renujan <roshanrenujan20020426@gmail.com>
2026-08-11 09:54:00 +0530

    01 Install DRF authentication dependencies

--- finance_tracker/settings.py
+++ finance_tracker/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Third-party applications
     'rest_framework',
+    'corsheaders',
     # Local applications
     'users',
 ]
@@ -50,11 +51,18 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# CORS Configuration
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 ROOT_URLCONF = 'finance_tracker.urls'
