a68952fcd3e56a6f2f3d584a6b0d059110850e74
Renujan <roshanrenujan20020426@gmail.com>
2026-08-11 09:55:20 +0530

    07 Add JWT refresh endpoint

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from authentication.views import RegisterView
 
 app_name = 'authentication'
@@ -7,4 +10,5 @@ app_name = 'authentication'
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
