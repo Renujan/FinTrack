7b19aced61dba03f80a87e64a05d74da0f6d529e
Renujan <roshanrenujan20020426@gmail.com>
2026-08-12 17:30:57 +0530

    Implement transaction category model

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,26 @@
+from django.conf import settings
 from django.db import models
 
-# Models will be implemented in subsequent steps.
+
+class Category(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='categories'
+    )
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+        ordering = ['name']
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'name'],
+                name='unique_user_category'
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.name} ({self.user})"
