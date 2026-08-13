6603e2519be280da33292a310822eee5c8bcbd30
Renujan <roshanrenujan20020426@gmail.com>
2026-08-13 09:49:32 +0530

    Improve transaction pagination

--- transactions/pagination.py
+++ transactions/pagination.py
@@ -2,6 +2,10 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class StandardResultsSetPagination(PageNumberPagination):
+    """
+    Standard pagination for API querysets with a default page size of 10
+    and maximum limit of 100 per page.
+    """
     page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 100
