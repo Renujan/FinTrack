21f96afd50e9e26fbfd0892b5bd127c5e5b8d915
Renujan <roshanrenujan20020426@gmail.com>
2026-08-13 09:51:45 +0530

    Complete Day 4 API quality and validation

--- transactions/apps.py
+++ transactions/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class TransactionsConfig(AppConfig):
+    """App configuration for transactions and category management module."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'transactions'
