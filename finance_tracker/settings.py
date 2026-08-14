51af9d64681227d62f287dfba8035ad2887fdd62
Renujan <roshanrenujan20020426@gmail.com>
2026-08-14 10:45:34 +0530

    feat: configure standardized API errors

--- finance_tracker/settings.py
+++ finance_tracker/settings.py
@@ -156,6 +156,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+    'EXCEPTION_HANDLER': 'finance_tracker.exceptions.custom_exception_handler',
 }
 
 # Simple JWT Configuration
