c6c2ae1ac8a0f811a1d6360411b922edf1b3bda1
Renujan <roshanrenujan20020426@gmail.com>
2026-08-18 09:27:40 +0530

    feat: add recurring transaction permissions

--- transactions/permissions.py
+++ transactions/permissions.py
@@ -3,8 +3,8 @@ from rest_framework import permissions
 
 class IsOwner(permissions.BasePermission):
     """
-    Object-level permission enforcement to only allow owners of Category, Transaction, or Budget
-    objects to access, edit, or delete them. Prevents unauthorized cross-user access.
+    Object-level permission enforcement to only allow owners of Category, Transaction, Budget,
+    or RecurringTransaction objects to access, edit, or delete them. Prevents unauthorized cross-user access.
     """
 
     def has_object_permission(self, request, view, obj):
